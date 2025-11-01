1abf49861ba1e67bec22685344493d493863bd62
ekard <105611677+helloplauz10@users.noreply.github.com>
2025-11-01 23:55:13 +0800

    fix unrecognized argument for the adaptive icon

--- pythonforandroid/toolchain.py
+++ pythonforandroid/toolchain.py
@@ -930,7 +930,7 @@ class ToolchainCL:
 
         fix_args = ('--dir', '--private', '--add-jar', '--add-source',
                     '--whitelist', '--blacklist', '--presplash', '--icon',
-                    '--icon-bg', '--icon-fg')
+                    '--icon-bg', '--icon-fg', '--icon-adaptive')
         unknown_args = args.unknown_args
 
         for asset in args.assets:
